dab2c071194bd0028cd04444fadfce1f07c09d2e
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-22 22:27:12 +0200

    Add script that starts a Flask web application listening on 0.0.0.0, port 5000 with two routes.

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,13 +8,13 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """Print 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Print 'HBNB'"""
     return 'HBNB'
