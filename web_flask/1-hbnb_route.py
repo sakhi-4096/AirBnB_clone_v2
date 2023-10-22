f912d7f638dbfe73fdc26b2d713e69e2b32d22b5
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-22 22:17:24 +0200

    Add script that starts a Flask web application listening on 0.0.0.0, port 5000.

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -8,11 +8,17 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route('/')
 def hello_hbnb():
     """Print 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
+@app.route('/hbnb')
+def hbnb():
+    """Print 'HBNB'"""
+    return 'HBNB'
+
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
