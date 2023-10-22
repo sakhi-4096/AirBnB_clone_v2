a9de0f9b251b29ed76cff735778776fbef4b8f64
Sakhile Lindah Ndlazi <sakhilelindah@protonmail.com>
2023-10-22 22:29:39 +0200

    Add /number/<n>: display “n is a number” only if n is an integer route.

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Run a Flask application with debugging enabled, listening on
+'0.0.0.0' and port 5000."""
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Print 'Hello HBNB!'"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Print 'HBNB'"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Print 'C ' followed by the text parameter with underscores replaced
+    by spaces"""
+    return f'C {text.replace("_", " ")}'
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is_cool"):
+    """Print 'Python 'followed by the text parameter or 'is_cool' by default"""
+    return f'Python {text.replace("_", " ")}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_n(n):
+    """Print 'n is a number' for an integer 'n'"""
+    return f'{n} is a number'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
